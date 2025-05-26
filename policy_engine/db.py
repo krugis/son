58b9053c30638c386837c62ee1489f83c0d285cd
krugis <158180572+krugis@users.noreply.github.com>
2025-05-26 16:13:44 +0300

    Update db.py

--- policy_engine/db.py
+++ policy_engine/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import DATABASE_URL
+from config import POLICY_DB_URL
 
 # Create the SQLAlchemy engine
 engine = create_engine(POLICY_DB_URL)
